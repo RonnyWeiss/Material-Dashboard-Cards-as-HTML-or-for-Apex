a6539218adf08f3901523e4b7586c425d7e07514
RonnyWeiss <ronnyweiss@outlook.com>
2019-02-25 20:28:31 +0100

    update license and concat all css and js files change index.html with apex font icons update util.js

--- data/data.js
+++ data/data.js
@@ -1,1 +1,112 @@
-var dataJSON = {};
+var dataJSON = {
+    "row": [
+        {
+            "CARD_TYPE": "icon",
+            "CARD_ICON": "fa-globe",
+            "CARD_ICON_COLOR": "",
+            "CARD_HEADER_STYLE": "",
+            "CARD_TITLE": "Material Card",
+            "CARD_VALUE": "<span style=\"color:#920505;\">7 %</span>",
+            "CARD_FOOTER": "This is a Material Card that shows an icon and links to ValueTech",
+            "CARD_LINK": "https://www.valuetech.de/",
+            "CARD_CHART_DATA": "",
+            "CARD_CHART_CONFIG": ""
+      },
+        {
+            "CARD_TYPE": "icon",
+            "CARD_ICON": "fa-gear",
+            "CARD_ICON_COLOR": "",
+            "CARD_HEADER_STYLE": "",
+            "CARD_TITLE": "Material Card",
+            "CARD_VALUE": "<span style=\"color:#920505;\">20 %</span>",
+            "CARD_FOOTER": "This is a Material Card that shows an icon and links to ValueTech",
+            "CARD_LINK": "https://www.valuetech.de/",
+            "CARD_CHART_DATA": "",
+            "CARD_CHART_CONFIG": ""
+      },
+        {
+            "CARD_TYPE": "icon",
+            "CARD_ICON": "fa-user",
+            "CARD_ICON_COLOR": "",
+            "CARD_HEADER_STYLE": "",
+            "CARD_TITLE": "Material Card",
+            "CARD_VALUE": "<span style=\"color:#920505;\">8 %</span>",
+            "CARD_FOOTER": "This is a Material Card that shows an icon and links to ValueTech",
+            "CARD_LINK": "https://www.valuetech.de/",
+            "CARD_CHART_DATA": "",
+            "CARD_CHART_CONFIG": ""
+      },
+        {
+            "CARD_TYPE": "chart-bar",
+            "CARD_ICON": "fa-bar-chart",
+            "CARD_ICON_COLOR": "",
+            "CARD_HEADER_STYLE": "background: linear-gradient(60deg, hsl(23, 55%, 60%), hsl(23, 50%, 60%));box-shadow: 0 12px 20px -10px rgba(230, 230, 230, 0.28), 0 4px 20px 0px rgba(0, 0, 0, 0.12), 0 7px 8px -5px rgba(230, 230, 230, 0.2);",
+            "CARD_TITLE": "Material Card with bar chart",
+            "CARD_VALUE": "100%",
+            "CARD_FOOTER": "This is a Material Card that shows a bar chart and links to ValueTech",
+            "CARD_LINK": "https://www.valuetech.de/",
+            "CARD_CHART_DATA": "{\n           \"labels\": [\"Mo\", \"Tue\", \"Wed\", \"Th\", \"Fr\"],\n            \"series\": [[4,2,2,2,4], \n                       [6,20,9,15,10],\n                       [9,19,9,2,2]]\n        }",
+            "CARD_CHART_CONFIG": ""
+      },
+        {
+            "CARD_TYPE": "chart-line",
+            "CARD_ICON": "fa-line-chart",
+            "CARD_ICON_COLOR": "",
+            "CARD_HEADER_STYLE": "",
+            "CARD_TITLE": "Material Card with line chart",
+            "CARD_VALUE": "100%",
+            "CARD_FOOTER": "This is a Material Card that shows a line chart and links to ValueTech",
+            "CARD_LINK": "https://www.valuetech.de/",
+            "CARD_CHART_DATA": "{\n          \"labels\": [\"Mo\", \"Tue\", \"Wed\", \"Th\", \"Fr\"],\n          \"series\": [[4,2,2,2,4]]\n        }",
+            "CARD_CHART_CONFIG": "{\n          \"low\": 0,\n          \"showArea\": true\n        }"
+      },
+        {
+            "CARD_TYPE": "chart-pie",
+            "CARD_ICON": "fa-pie-chart",
+            "CARD_ICON_COLOR": "",
+            "CARD_HEADER_STYLE": "",
+            "CARD_TITLE": "Material Card with pie chart",
+            "CARD_VALUE": "100%",
+            "CARD_FOOTER": "This is a Material Card that shows a pie chart and links to ValueTech",
+            "CARD_LINK": "https://www.valuetech.de/",
+            "CARD_CHART_DATA": "{\n          \"labels\": [\"Mo\", \"Tue\", \"Wed\", \"Th\", \"Fr\"],\n          \"series\": [4,2,2,2,4]\n       }",
+            "CARD_CHART_CONFIG": ""
+      },
+        {
+            "CARD_TYPE": "chart-pie",
+            "CARD_ICON": "fa-pie-chart",
+            "CARD_ICON_COLOR": "",
+            "CARD_HEADER_STYLE": "",
+            "CARD_TITLE": "Material Card with pie chart",
+            "CARD_VALUE": "70 %",
+            "CARD_FOOTER": "This is a Material Card that shows a gauge chart and links to ValueTech",
+            "CARD_LINK": "https://www.valuetech.de/",
+            "CARD_CHART_DATA": "{\n          \"labels\": [\"70 %\"],\n          \"series\": [70]\n       }",
+            "CARD_CHART_CONFIG": "{\n          \"total\": 100,\n          \"donut\": true,\n          \"donutWidth\": 260,\n          \"sliceWidth\": 10\n        }"
+      },
+        {
+            "CARD_TYPE": "chart-bar",
+            "CARD_ICON": "fa-bar-chart",
+            "CARD_ICON_COLOR": "",
+            "CARD_HEADER_STYLE": "",
+            "CARD_TITLE": "Material Card with bar chart",
+            "CARD_VALUE": "100%",
+            "CARD_FOOTER": "This is a Material Card that shows a horizontal bar chart and links to ValueTech",
+            "CARD_LINK": "https://www.valuetech.de/",
+            "CARD_CHART_DATA": "{\n           \"labels\": [\"Mo\", \"Tue\", \"We\", \"Th\", \"Fr\"],\n            \"series\": [[4,2,2,2,4]]\n        }",
+            "CARD_CHART_CONFIG": "{\n            \"horizontalBars\": true,\n            \"chartPadding\": {\n                \"top\": 5\n            },\n            \"strokeWidth\": 12,\n            \"low\": 0,\n            \"high\": 5\n        }"
+      },
+        {
+            "CARD_TYPE": "chart-line",
+            "CARD_ICON": "fa-line-chart",
+            "CARD_ICON_COLOR": "",
+            "CARD_HEADER_STYLE": "",
+            "CARD_TITLE": "Material Card with line chart",
+            "CARD_VALUE": "100%",
+            "CARD_FOOTER": "This is a Material Card that shows a scatter chart and links to ValueTech",
+            "CARD_LINK": "https://www.valuetech.de/",
+            "CARD_CHART_DATA": "{\n          \"labels\": [\"Mo\", \"Tue\", \"Wed\", \"Th\", \"Fr\"],\n          \"series\": [[4,2,2,2,4], \n                       [6,20,9,15,10],\n                       [9,19,9,2,2]]\n        }",
+            "CARD_CHART_CONFIG": "{\n          \"showArea\": false,\n          \"showLine\": true,\n          \"showPoint\": true,\n          \"fullWidth\": true,\n          \"axisX\": {\n                     \"showLabel\": false,\n                     \"showGrid\": false\n                     }\n        }"
+      }
+    ]
+};
